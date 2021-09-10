dfdacb50dcfe60876ba7d476818b03de88e15c72
hekun <hekun4@xdf.cn>
2021-09-10 19:04:47 +0800

    read file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 struct stBox {
-  uint64_t boxLen;
+  uint32_t boxLen;
+  uint64_t boxLargeLen;
   uint8_t boxType[4];
   uint8_t* boxData;
 };
@@ -13,33 +15,56 @@ int main() {
   std::string strInput = "../../../test.mp4";
   FILE* fp = fopen(strInput.c_str(), "rb");
   if (fp) {
-    uint nReadUnit = 2048 * 1000;
-    uint8_t* szReadBuf = new uint8_t[nReadUnit];
+    const uint nReadOnce = 2048 * 1000;
+    uint8_t* szReadBuf = new uint8_t[nReadOnce];
     fseek(fp, 0, SEEK_END);
     uint fileSize = ftell(fp);
     cout << "file size is " << fileSize << endl;
     fseek(fp, 0, SEEK_SET);
-    
     int nOffSet = 0;
     int nBoxDatalen = 0;
+    uint64_t nMaxDataSize = 400*1000;
     stBox box;
-    
-    while(fileSize > 0) {
-      fread(szReadBuf, 1, nReadUnit, fp);
-      
-      box.boxLen = szReadBuf[nOffSet+0] | szReadBuf[nOffSet+1] |
-      szReadBuf[nOffSet+2] | szReadBuf[nOffSet+3];
-      nOffSet+=4;
+    box.boxData = new uint8_t[nMaxDataSize];
+    int nFileOffset = 0;
+    int nReadUnit = nReadOnce;
+    int nLeftToUse = 0;
+    while(nFileOffset < fileSize) {
+      //循环读取直到文件尾
+      nReadUnit = std::min(fileSize - nFileOffset, nReadOnce);
+      nLeftToUse = nReadOnce - nOffSet;
+      nReadUnit -= nLeftToUse;
+      memmove(szReadBuf, szReadBuf+nOffSet, nLeftToUse);
+      fread(szReadBuf+nLeftToUse, 1, nReadUnit, fp);
+      nOffSet = 0;
+      //size 一下把4字节长度和可能的largeSize都读取了以及type
+      if(nReadOnce - nOffSet >= 16) {
+      memcpy((void*)&box.boxLen, szReadBuf, 4);
+      box.boxLen = szReadBuf[nOffSet+0] | szReadBuf[nOffSet+1] | szReadBuf[nOffSet+2] | szReadBuf[nOffSet+3];
+      nOffSet += 4;
+      if(box.boxLen == 1) {
+        memcpy((void*)&box.boxLargeLen, szReadBuf, 8);
+        nOffSet += 8;
+      }
+      //type
       memcpy(box.boxType, szReadBuf+nOffSet, 4);
       nOffSet += 4;
-      nBoxDatalen = box.boxLen - 8;
-      box.boxData = new uint8_t[nBoxDatalen];
+      }
+      //data
+      nBoxDatalen = box.boxLen - 4 - (box.boxLen==1 ? 4 : 8);
+      if(box.boxLen > nMaxDataSize || box.boxLargeLen > nMaxDataSize)
+      {
+        nMaxDataSize = std::max((uint64_t)box.boxLen, box.boxLargeLen);
+        delete[] box.boxData;
+        box.boxData = new uint8_t[nMaxDataSize];
+      }
       memcpy(box.boxData, szReadBuf+nOffSet, nBoxDatalen);
-      
       printf("box type: %s, size:%lld, dataLen:%d\n", box.boxType, box.boxLen, nBoxDatalen);
+#if 0 //暂时不显示
       for(int i = 0;i < nBoxDatalen; i++) {
         printf("%02x ", box.boxData[i]);
       }
+#endif
       printf("\n");
     }
     fclose(fp);
